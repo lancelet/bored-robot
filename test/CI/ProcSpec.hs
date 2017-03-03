7c43e29e2832f99c9a4fc69ecf1f525195026c7d
Jonathan Merritt <j.s.merritt@gmail.com>
2017-03-03 21:06:28 +1100

    Proc effect, and Git in terms of Proc

---
+++ test/CI/ProcSpec.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module CI.ProcSpec
+    ( test
+    ) where
+
+import CI.Proc
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Control.Monad.Eff
+import Control.Monad.Eff.Exception
+import Control.Monad.Eff.Lift
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString as BS
+
+test :: TestTree
+test = testGroup "ProcSpec Tests"
+    [ stackVersionTest ]
+
+-- | NB: This test ACTUALLY occurs in IO.
+stackVersionTest :: TestTree
+stackVersionTest = testCase "invoke proc to get stack version" testAction
+  where
+
+    testAction :: IO ()
+    testAction = do
+        r <- action
+        case r of
+            Left _ -> assertFailure "Expected: \"Version\"..."
+            Right str -> assertBool "Expected: \"Version\"..."
+                         $ T.isPrefixOf "Version" str
+        return ()
+
+    action :: IO (Either ProcEx Text)
+    action = runLift $ runException $ runProc $ getStackVersion
+
+    getStackVersion :: ( Member Proc r,
+                         Member (Exception ProcEx) r )
+                    => Eff r Text
+    getStackVersion = do
+        (stdout, _) <- proc "stack" ["--version"] (Stdin BS.empty)
+        return $ T.decodeUtf8 $ unStdout stdout
