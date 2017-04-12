e70d524274d346db3580210a04440972fb4ef8d0
Thomas Sutton <me@thomas-sutton.id.au>
2017-04-12 12:02:05 +1000

    Rework Docker tests to use Trace interpreter

--- test/CI/DockerSpec.hs
+++ test/CI/DockerSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module CI.DockerSpec
@@ -8,23 +8,23 @@ module CI.DockerSpec
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Data.Monoid
-import Control.Monad.Eff
-import Control.Monad.Eff.Exception
-import Control.Monad.Eff.Lift
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import qualified Data.ByteString as BS
+import           Control.Monad.Eff
+import           Control.Monad.Eff.Exception
+import           Control.Monad.Eff.Lift
+import           Control.Monad.Eff.Trace
+import qualified Data.ByteString             as BS
+import           Data.Monoid
+import           Data.Text                   (Text)
+import qualified Data.Text                   as T
+import qualified Data.Text.Encoding          as T
 
 import CI.Docker
-import CI.Proc
+import CI.Filesystem
 
 
 test :: TestTree
 test = testGroup "DockerSpec Tests"
-    [ deleteImageTest
-    , tagMissingImageTest
+    [ buildAndTag
     ]
 
 assertMissingImageError expected error =
@@ -33,52 +33,31 @@ assertMissingImageError expected error =
         _ -> assertFailure $ "Expected NoSuchImage error but got " <> show error
 
 -- | NB: This test ACTUALLY occurs in IO.
-deleteImageTest :: TestTree
-deleteImageTest = testCase "delete a [missing] image" testAction
+buildAndTag :: TestTree
+buildAndTag = testCase "build and tag" testAction
   where
     testAction :: IO ()
     testAction = do
-        r <- action
-        case r of
-            Left err -> assertFailure $ "Couldn't run commands: " <> show err
-            Right (Left err) -> assertMissingImageError target err
-            Right (Right ())  -> assertFailure "Expected to fail"
+        case action of
+            (Right img, tr) -> assertBool ("Expected to get " <> show target <> " but got " <> show img) (target == img)
+            (Left err, tr) -> assertFailure $ "Failed to execute commands: " <> show err <> unlines tr
         return ()
 
-    action :: IO (Either ProcEx (Either DockerEx ()))
-    action = runLift $ runException $ runException $ runProc $ runDocker $ deleteAnImage
+    action :: (Either DockerEx Image, [String])
+    action = run . runTracePure . runException . runDockerTrace $ doiit
 
-    target :: Image
-    target = Image "ahahahahahah" (Tag "hullo")
+    source = Image "ahahahahahah" (Tag "latest")
 
-    deleteAnImage :: ( Member Proc r
-                    , Member Docker r
-                    , Member (Exception DockerEx) r )
-                    => Eff r ()
-    deleteAnImage = removeImage target
+    target = Image "ahahahahahah" (tag)
 
--- | NB: This test ACTUALLY occurs in IO.
-tagMissingImageTest :: TestTree
-tagMissingImageTest = testCase "tag a [missing] image" testAction
-  where
-    testAction :: IO ()
-    testAction = do
-        r <- action
-        case r of
-            Left err -> assertFailure $ "Couldn't run commands: " <> show err
-            Right (Left err) -> assertMissingImageError target err
-            Right (Right (Image{}))  -> assertFailure "Expected to fail"
-        return ()
+    tag = Tag "excrescence"
 
-    action :: IO (Either ProcEx (Either DockerEx Image))
-    action = runLift $ runException $ runException $ runProc $ runDocker $ tagAnImage
-
-    target :: Image
-    target = Image "ahahahahahah" (Tag "what")
-
-    tagAnImage :: ( Member Proc r
-                 , Member Docker r
-                 , Member (Exception DockerEx) r )
-                 => Eff r Image
-    tagAnImage = tagImage target (Tag "excrescence")
+    doiit :: ( Member Docker r
+            , Member (Exception DockerEx) r
+            , Member Trace r)
+            => Eff r Image
+    doiit = do
+        img <- buildImage (filePathToPath "hello/my/friend") source
+        img' <- tagImage img tag
+        return img'
 
